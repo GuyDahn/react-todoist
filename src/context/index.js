504494192332c830144c06677fc60db30d7a0d17
GuyDahn <guydaane@gmail.com>
2020-04-08 23:48:57 +0300

    added context

---
+++ src/context/index.js
@@ -0,0 +1,20 @@
+import {
+    ProjectsContext,
+    ProjectsProvider,
+    useProjectsValue
+} from "./projects-context";
+
+import {
+    SelectedProjectContext,
+    SelectedProjectProvider,
+    SelectedProjectValue
+} from "./selected-projects-context";
+
+export {
+    ProjectsContext,
+    ProjectsProvider,
+    useProjectsValue,
+    SelectedProjectContext,
+    SelectedProjectProvider,
+    SelectedProjectValue
+}
--- src/context/project-context.js
+++
@@ -1,3 +0,0 @@
-import React, { createContext, useContext } from "react";
-import { useProjects } from '../hooks'
-
